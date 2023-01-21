c0de934c38895de6b2363da830dde869aa848b90
Raghav Srinivasan <rsrinivasan2021@gmail.com>
2023-01-21 14:56:00 -0500

    able to save audio

--- UofTHacks/App.js
+++ UofTHacks/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { Logs } from "expo";
 import { Audio } from "expo-av";
 
-Logs.enableExpoCliLogging();
 import {
 	StyleSheet,
 	Dimensions,
@@ -13,54 +12,64 @@ import {
 
 import React, { useState } from "react";
 
+Logs.enableExpoCliLogging();
+
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 
-const recording = false;
+let recordingOngoing = false;
 
 export default function App() {
 	const [recording, setRecording] = useState(null);
 	const [timerText, setTimerText] = useState("Press record to start!");
-	const startRecording = async () => {
-		console.log("start recording");
+
+	async function startRecording() {
+		recordingOngoing = true;
 		setTimerText("Recording...");
-		recording = true;
 		try {
-			const permission = await Audio.requestPermissionsAsync();
-			if (permission.status === "granted") {
-				await Audio.setAudioModeAsync({
-					allowsRecordingIOS: true,
-					playsInSilentModeIOS: true,
-				});
-				const recording = new Audio.Recording().createAsync(
-					Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-				);
-				setRecording(recording);
-			} else {
-				console.log("Please allow permission to record audio!");
-			}
-		} catch (error) {
-			console.log(error);
+			console.log("Requesting permissions..");
+			await Audio.requestPermissionsAsync();
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: true,
+				playsInSilentModeIOS: true,
+			});
+
+			console.log("Starting recording..");
+			const { recording } = await Audio.Recording.createAsync(
+				Audio.RecordingOptionsPresets.HIGH_QUALITY
+			);
+			setRecording(recording);
+			console.log("Recording started");
+		} catch (err) {
+			console.error("Failed to start recording", err);
 		}
-	};
-	const stopRecording = async () => {
-		console.log("stop recording");
+	}
+
+	async function stopRecording() {
+		recordingOngoing = false;
 		setTimerText("Press record to start!");
-		recording = false;
-		try {
-			setRecording(undefined);
-			await recording.stopAndUnloadAsync();
-		} catch (error) {}
-	};
+		console.log("Stopping recording..");
+		setRecording(undefined);
+		await recording.stopAndUnloadAsync();
+		await Audio.setAudioModeAsync({
+			allowsRecordingIOS: false,
+		});
+		const uri = recording.getURI();
+		getAverageAmplitudefromAudio();
+	}
+
+	const getAverageAmplitudefromAudio = async () => {};
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>UofTSoundSpace</Text>
 			<Text style={styles.timer}>{timerText}</Text>
 			<TouchableOpacity
 				style={styles.recordButton}
-				onPress={recording ? startRecording : stopRecording}
+				onPress={recordingOngoing ? stopRecording : startRecording}
 			>
-				<View style={styles.record}></View>
+				<View
+					style={recordingOngoing ? styles.stop : styles.record}
+				></View>
 			</TouchableOpacity>
 			<StatusBar style="auto" />
 		</View>
@@ -92,6 +101,14 @@ const styles = StyleSheet.create({
 		height: width * 0.3,
 		borderRadius: width * 0.15,
 		backgroundColor: "white",
+	},
+	stop: {
+		marginTop: width * 0.1,
+		marginLeft: width * 0.1,
+		width: width * 0.1,
+		height: width * 0.1,
+		borderRadius: 0,
+		backgroundColor: "rgb(255, 70, 70)",
 	},
 	record: {
 		marginTop: width * 0.075,
