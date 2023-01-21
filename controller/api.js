8a346745ec13980976defb9910e749799677164e
Rudy <rudraksh.monga@mail.utoronto.ca>
2023-01-21 18:48:10 -0500

    testing

--- controller/api.js
+++ controller/api.js
@@ -2,20 +2,24 @@ const axios = require('axios');
 const apiRouter = require('express').Router();
 
 apiRouter.get('/allLocations', async (req, res) => {
-    axios({
-        url: "https://uofthacksx.onrender.com/graphql",
-        method: 'get',
-        data: {
-            query: `
-                query getAllLocations {
+    fetch("https://uofthacksx.onrender.com/graphql", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            query:`
+                getAllLocations {
                     id
                     address
                     soundBiteIDs
                 }
             `
-        }
+        })
     }).then((result) => {
-        console.log(result.data);
+        console.log(result.json());
+        res.send(result.json());
     })
 });
 
